900eb34b6b51702fe4d3918d5f310adfd9a7bc17
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-10-04 14:25:49 +0530

    Add install targets

--- src/main.c
+++ src/main.c
@@ -3,5 +3,27 @@
 int main() {
     printf("yeah, mate!\n");
 
-    return 0;
+int main(int argc, char **argv)
+{
+  struct arguments arguments;
+  arguments.mode = CLIENT_MODE;
+  arguments.silent = false;
+  arguments.verbose = false;
+  arguments.string = "[FILENAME]...";
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
+  switch (arguments.mode)
+  {
+  case CLIENT_MODE:
+    printf("Starting rsync in client mode...\n");
+    printf("ARGS %s\n", arguments.string);
+    break;
+  case DAEMON_MODE:
+    printf("Starting the rsync daemon...\n");
+    execlp("rsync", "--help");
+    break;
+  case PASSTHROUGH_MODE:
+    printf("Starting rsync through passthrough...\n");
+    break;
+  }
+  return 0;
 }
