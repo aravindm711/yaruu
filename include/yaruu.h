918f3bed428e612110f0bbef0ea05d9375766641
Aravind Murali <aravindmurali711@gmail.com>
2019-10-24 19:42:40 +0530

    Modified yaruu.h to make it work for splitting files

--- include/yaruu.h
+++ include/yaruu.h
@@ -8,7 +8,9 @@
 #ifndef YARUU_H
 #define YARUU_H
 
-void split_file(char **);
+void split_file(char**);
+void create_dir(char*);
+void remove_dir(char*);
 
 int run_client(char **arg, size_t *arg_len)
 {
@@ -33,39 +35,59 @@ off_t file_size(const char* file_path) {
     if (stat(file_path, &st) == 0)
         return st.st_size;
 
-    fprintf(stderr, "Cannot determine size of %s: %s\n",
-            file_path, strerror(errno));
+    // fprintf(stderr, "Cannot determine size of %s: %s\n",
+    //         file_path, strerror(errno));
 
     return -1;
 }
 
+void remove_dir(char* dir) {
+    struct stat st = {0};
+    if (stat(dir, &st) == 0) {
+        char command[40];
+        sprintf(command, "rm -rf %s", dir);
+        system(command);
+    }
+}
+
+void create_dir(char* dir) {
+    struct stat st = {0};
+    if (stat(dir, &st) == -1) {
+        mkdir(dir, 0777);
+    }
+}
+
 void split_file(char** arg) {
-    char buffer[10];
+    char* yaruu_split_dir;
+    char* split_command;
+    char final_command[100];
+    char os[10];
+
     FILE* os_info;
     os_info = popen("/usr/bin/uname", "r");
     if (os_info != NULL) {
         if (!feof(os_info)) {
-            fgets(buffer, 10, os_info);
+            fgets(os, 10, os_info);
         }
         pclose(os_info);
     }
 
-    char* os = buffer;
     if (strncmp(os, "Darwin", 6) == 0) {
-        printf("yep");
-    } else if (strncmp(buffer, "Linux", 5) == 0) {
-        struct stat st = {0};
-        if (stat("/tmp/yaruu-split-dir/", &st) == -1) {
-            mkdir("/tmp/yaruu-split-dir/", 0777);
-        }
-
-        char* split_command = (char *)"split ";
-        const char* split_dir = " /var/yaruu-split-dir/split_file_";
-        split_command = (char *)realloc(split_command,sizeof(split_command)+sizeof(arg[0])+ sizeof(split_dir));
-        split_command = strcat(split_command, arg[0]);
-        split_command = strcat(split_command, split_dir);        
-        system(split_command);
+        split_command = (char *)"gsplit";
+        yaruu_split_dir = (char *)"/private/tmp/yaruu-split-dir/";
+    } else if (strncmp(os, "Linux", 5) == 0) {
+        split_command = (char *)"split";
+        yaruu_split_dir = (char *)"/tmp/yaruu-split-dir/";
+    } else {
+        printf("Not a Darwin/Linux system. Get one, to use this command.\n");
+        exit(0);
     }
+
+    create_dir(yaruu_split_dir);
+    snprintf(final_command, 100, "%s %s %ssplitfile_", split_command, arg[0], yaruu_split_dir);
+    system(final_command);
+    printf("running: %s\n", final_command);
+    remove_dir(yaruu_split_dir);
 }
 
 #endif // YARUU_H
